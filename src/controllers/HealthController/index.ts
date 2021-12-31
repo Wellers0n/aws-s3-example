937859900473017725ec4cd59453d37b49d9d4ae
Wellerson <wllrsn.c@gmail.com>
2021-12-31 14:03:07 -0300

    fix: prettier

--- src/controllers/HealthController/index.ts
+++ src/controllers/HealthController/index.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from 'express'
+import {Request, Response} from 'express'
 
 const HealthController = (request: Request, response: Response) => {
-    return response.status(200).json({message: 'funcionou'})
+    return response.status(200).json({message: 'ok'})
 }
 
 export default HealthController
